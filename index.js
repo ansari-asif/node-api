e47bb52a36ac3b70008eed3ba063abed7ffd2453
Asif Ansari <57432717+ansari-asif@users.noreply.github.com>
2022-08-29 17:18:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,38 @@ const user=require('./db/User');
 
 const app=express()
 app.use(express.json());
-app.get("/register",(req,res)=>{
-    res.send(req.body);
+app.post("/register",async(req,res)=>{
+    let user=new User(req.body)
+    let result=await user.save();
+    if (result) {
+        jwt.sign({result},jwt_key,{expiresIn:"2h"},(err,token)=>{
+            if (err) {
+                res.send('something Went wrong');
+            }
+            res.send({result,auth:token})
+        })
+    }else{
+        res.send({result:"something went wrong"})
+    }
+});
+
+app.post("/login",async(req,res)=>{
+    if (req.body.email && req.body.password) {        
+        
+        let user=await User.findOne(req.body).select("-password");
+        if (user) {
+            jwt.sign({user},jwt_key,{expiresIn:"2h"},(err,token)=>{
+                if (err) {
+                    res.send('something Went wrong');
+                }
+                res.send({user,auth:token})
+            })
+        }else{
+            res.send({result: "No user found"})
+        }
+    }else{
+        res.send({result: "Invalid Parameter"})
+    }
 });
 
 app.listen(5000);
